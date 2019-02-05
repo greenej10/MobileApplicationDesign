45fb9d125409ab8aa497f72bdce40a60a984ff9f
Mohaimenur Rahman Bivor <abm.bivor@gmail.com>
2019-02-04 21:49:57 -0500

    Main activity update

--- Group14_InClass03/app/src/main/java/net/ddns/bivor/group14_inclass03/MainActivity.java
+++ Group14_InClass03/app/src/main/java/net/ddns/bivor/group14_inclass03/MainActivity.java
@@ -1,13 +1,116 @@
 package net.ddns.bivor.group14_inclass03;
 
+import android.content.Intent;
+import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    EditText firstName, lastName, studentID;
+    String department;
+    RadioGroup rg;
+    RadioButton rb;
+    public static final int REQ_CODE = 100;
+    public static final int REQ_CODE2 = 101;
+    public static final String AVATAR_KEY = "AVATAR_KEY";
+    public static final String STUDENT_KEY = "STUDENT_KEY";
+    int avatar;
+    ImageView iv;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        firstName = findViewById(R.id.editTextFirstName);
+        lastName = findViewById(R.id.editTextLastName);
+        studentID = findViewById(R.id.editTextStudentID);
+
+        iv = findViewById(R.id.imageViewMain);
+
+        rg = findViewById(R.id.radioGroup);
+        rg.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(RadioGroup group, int checkedId) {
+
+                if(checkedId == -1){
+                    department = "empty";
+                }
+                else {
+                    rb = findViewById(checkedId);
+                    department = rb.getText().toString();
+                }
+
+            }
+        });
+
+        iv.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, SelectAvatar.class);
+                startActivityForResult(intent, REQ_CODE);
+            }
+        });
+
+        findViewById(R.id.buttonSave).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                if (firstName.getText().toString().isEmpty()){
+                    firstName.setError("Enter your First Name!");
+                }
+                else if (lastName.getText().toString().isEmpty()){
+                    lastName.setError("Enter your Last Name!");
+                }
+                else if (studentID.getText().toString().isEmpty()){
+                    studentID.setError("Enter your Student ID!");
+                }
+                else if (studentID.getText().toString().length()!=9)
+                {
+                    studentID.setError("Student ID should be of 9 Digits");
+                }
+                else if (department.equals("empty")){
+
+                    Toast.makeText(MainActivity.this, "Select a Department!", Toast.LENGTH_SHORT).show();
+                }
+                else
+                {
+                    String first = firstName.getText().toString();
+                    String last = lastName.getText().toString();
+                    String studentNo = studentID.getText().toString();
+                    String avatarID = Integer.toString(avatar);
+
+                    Intent intent = new Intent(MainActivity.this, Display.class);
+                    intent.putExtra(STUDENT_KEY, new Student(avatarID, first, last, studentNo,department));
+                    startActivityForResult(intent, REQ_CODE2 );
+                }
+
+
+            }
+        });
+
     }
+
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (requestCode == REQ_CODE){
+            if(resultCode == RESULT_OK){
+                   avatar = Integer.parseInt(data.getExtras().getString(AVATAR_KEY));
+                   iv.setImageResource(avatar);
+            }
+        }
+    }
+
+
 }
