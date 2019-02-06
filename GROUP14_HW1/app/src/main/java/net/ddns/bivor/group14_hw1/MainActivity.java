9276584e126722b7c96e8dd1e6cdee144a17cf48
Mohaimenur Rahman Bivor <abm.bivor@gmail.com>
2019-02-05 22:16:47 -0500

    GUI Almost Done without the ICON Issue

--- GROUP14_HW1/app/src/main/java/net/ddns/bivor/group14_hw1/MainActivity.java
+++ GROUP14_HW1/app/src/main/java/net/ddns/bivor/group14_hw1/MainActivity.java
@@ -1,13 +1,70 @@
 package net.ddns.bivor.group14_hw1;
 
+import android.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.CompoundButton;
+import android.widget.SeekBar;
+import android.widget.TextView;
+import android.widget.ToggleButton;
+import android.widget.Toolbar;
 
 public class MainActivity extends AppCompatActivity {
+
+    ToggleButton tb;
+    TextView textViewAlcohol;
+    SeekBar sb;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        android.support.v7.widget.Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        toolbar.setTitleTextColor(getResources().getColor(R.color.white));
+        getSupportActionBar().setTitle("BAC Calculator");
+
+        //toolbar.setLogo(R.mipmap.ic_launcher_foreground);
+
+
+
+        tb = findViewById(R.id.toggleButtonGender);
+        sb = findViewById(R.id.seekBar);
+        textViewAlcohol = findViewById(R.id.textViewAlcohol);
+//        tb.setTextOff("Female"); tb.setTextOn("Male");
+//        t = findViewById(R.id.textView4);
+//        tb.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+//            @Override
+//            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+//                    if(isChecked == false)t.setText("F");
+//                    else t.setText("M");
+//            }
+//        });
+
+        sb.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            int progressChangedValue = 0;
+
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+                progressChangedValue = progress*5;
+                textViewAlcohol.setText(""+progressChangedValue+" %");
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+
+
+
+            }
+        });
+
     }
 }
