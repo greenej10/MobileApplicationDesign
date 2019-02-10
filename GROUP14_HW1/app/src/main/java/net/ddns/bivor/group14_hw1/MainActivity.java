5def430f17543201e68f117717b85aa997213a12
greenej10 <jacobgreene.rfp@gmail.com>
2019-02-09 22:52:36 -0500

    Jacob Sat

--- GROUP14_HW1/app/src/main/java/net/ddns/bivor/group14_hw1/MainActivity.java
+++ GROUP14_HW1/app/src/main/java/net/ddns/bivor/group14_hw1/MainActivity.java
@@ -4,17 +4,37 @@ import android.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.CompoundButton;
+import android.widget.EditText;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
 import android.widget.SeekBar;
 import android.widget.TextView;
+import android.widget.Toast;
 import android.widget.ToggleButton;
 import android.widget.Toolbar;
 
 public class MainActivity extends AppCompatActivity {
 
     ToggleButton tb;
-    TextView textViewAlcohol;
+    TextView textViewAlcohol, textViewBACLevel;
     SeekBar sb;
+    EditText editTextWeight;
+    Button buttonSave;
+    Button buttonAddDrink;
+    Button buttonReset;
+    TextView t;
+    double gender;
+    float weight;
+    double BAC;
+    double percent;
+    float size;
+    RadioGroup rg;
+    RadioButton rb;
+    int rbID;
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -30,19 +50,50 @@ public class MainActivity extends AppCompatActivity {
         //toolbar.setLogo(R.mipmap.ic_launcher_foreground);
 
 
-
+        editTextWeight = findViewById(R.id.editText);
+        buttonSave = findViewById(R.id.buttonSave);
+        buttonAddDrink = findViewById(R.id.buttonAddDrink);
+        buttonReset = findViewById(R.id.buttonReset);
         tb = findViewById(R.id.toggleButtonGender);
         sb = findViewById(R.id.seekBar);
         textViewAlcohol = findViewById(R.id.textViewAlcohol);
-//        tb.setTextOff("Female"); tb.setTextOn("Male");
-//        t = findViewById(R.id.textView4);
-//        tb.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
-//            @Override
-//            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
-//                    if(isChecked == false)t.setText("F");
-//                    else t.setText("M");
-//            }
-//        });
+        tb.setTextOff("Female"); tb.setTextOn("Male");
+        t = findViewById(R.id.textView4);
+        textViewBACLevel = findViewById(R.id.textViewBACLevel);
+
+
+
+        tb.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                    if(isChecked == false)t.setText("F");
+                    else t.setText("M");
+            }
+        });
+
+        buttonSave.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                if(editTextWeight.getText().toString().equals("")){
+                    editTextWeight.setError("Enter Weight in Lbs!");
+                }
+
+                else {
+                    weight = Float.parseFloat(editTextWeight.getText().toString());
+                }
+
+                if(t.getText().equals("M")){
+                    gender = .68;
+                }
+                else {
+                    gender = .55;
+                }
+
+
+            }
+        });
+
 
         sb.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
             int progressChangedValue = 0;
@@ -51,6 +102,7 @@ public class MainActivity extends AppCompatActivity {
             public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
                 progressChangedValue = progress*5;
                 textViewAlcohol.setText(""+progressChangedValue+" %");
+                percent = progressChangedValue;
             }
 
             @Override
@@ -64,7 +116,50 @@ public class MainActivity extends AppCompatActivity {
 
 
             }
+
+
         });
+
+        rg = findViewById(R.id.radioGroup);
+        rg.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(RadioGroup group, int checkedId) {
+
+                if(checkedId == -1){
+
+                }
+                else {
+                    rb = findViewById(checkedId);
+                    size = Float.parseFloat(rb.getText().toString());
+                    rbID = checkedId;
+                }
+
+            }
+        });
+
+        buttonAddDrink.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                double drinkBAC;
+                drinkBAC = (size * percent)/(weight * gender);
+                BAC = BAC + drinkBAC;
+
+                textViewBACLevel.setText("" + BAC);
+
+            }
+        });
+
+        buttonReset.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                BAC = 0;
+
+                textViewBACLevel.setText("" + BAC);
+
+            }
+        });
+
 
     }
 }
