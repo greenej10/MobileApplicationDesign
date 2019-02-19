49add226061aee95ffb9adc83661aa59426bebea
Mohaimenur Rahman Bivor <abm.bivor@gmail.com>
2019-02-18 19:42:16 -0500

    CheckingConnectionMethod Implemented

--- Group14_InClass05/app/src/main/java/net/ddns/bivor/group14_inclass04/MainActivity.java
+++ Group14_InClass05/app/src/main/java/net/ddns/bivor/group14_inclass04/MainActivity.java
@@ -1,5 +1,8 @@
 package net.ddns.bivor.group14_inclass04;
 
+import android.content.Context;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
@@ -9,5 +12,17 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    private boolean isConnected(){
+        ConnectivityManager connectivityManager = (ConnectivityManager) getSystemService(Context.CONNECTIVITY_SERVICE);
+        NetworkInfo networkInfo = connectivityManager.getActiveNetworkInfo();
+        if(networkInfo == null || !networkInfo.isConnected() ||
+                (networkInfo.getType()!=ConnectivityManager.TYPE_WIFI
+                && networkInfo.getType()!=ConnectivityManager.TYPE_MOBILE)){
+            return false;
+        }
+        return true;
+
     }
 }
