963fb250b5170059924fc89b4ac5eaa6ee67e509
Mohaimenur Rahman Bivor <abm.bivor@gmail.com>
2019-03-18 20:02:57 -0400

    AddExpense Fragment UI done

---
+++ Group14_InClass07/app/src/main/java/net/ddns/bivor/group14_inclass07/AddExpenseFragment.java
@@ -0,0 +1,102 @@
+package net.ddns.bivor.group14_inclass07;
+
+import android.content.Context;
+import android.content.DialogInterface;
+import android.net.Uri;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.support.v7.app.AlertDialog;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+
+
+/**
+ * A simple {@link Fragment} subclass.
+ * Activities that contain this fragment must implement the
+ * {@link AddExpenseFragment.OnFragmentInteractionListener} interface
+ * to handle interaction events.
+ */
+public class AddExpenseFragment extends Fragment {
+
+    ArrayList<String> category;
+    TextView textViewCategory;
+
+    private OnFragmentInteractionListener mListener;
+
+    public AddExpenseFragment() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        category = new ArrayList<>(Arrays.asList("Groceries","Invoice","Transportation","Shopping", "Rent", "Trips",
+                "Utilities","Other"));
+
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_add_expense, container, false);
+    }
+
+    // TODO: Rename method, update argument and hook method into UI event
+    public void onButtonPressed(Uri uri) {
+        if (mListener != null) {
+            mListener.onFragmentInteraction(uri);
+        }
+    }
+
+    @Override
+    public void onAttach(Context context) {
+        super.onAttach(context);
+        if (context instanceof OnFragmentInteractionListener) {
+            mListener = (OnFragmentInteractionListener) context;
+        } else {
+            throw new RuntimeException(context.toString()
+                    + " must implement OnFragmentInteractionListener");
+        }
+    }
+
+    @Override
+    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+
+        AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
+
+        builder.setTitle("Choose a Keyword")
+                .setItems(category.toArray(new CharSequence[category.size()]), new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        textViewCategory.setText(category.get(which));
+                    }
+                });
+        final AlertDialog alertDialog = builder.create();
+        alertDialog.show();
+    }
+
+    @Override
+    public void onDetach() {
+        super.onDetach();
+        mListener = null;
+    }
+
+    /**
+     * This interface must be implemented by activities that contain this
+     * fragment to allow an interaction in this fragment to be communicated
+     * to the activity and potentially other fragments contained in that
+     * activity.
+     * <p>
+     * See the Android Training lesson <a href=
+     * "http://developer.android.com/training/basics/fragments/communicating.html"
+     * >Communicating with Other Fragments</a> for more information.
+     */
+    public interface OnFragmentInteractionListener {
+        // TODO: Update argument type and name
+        void onFragmentInteraction(Uri uri);
+    }
+}
--- Group14_InClass07/app/src/main/java/net/ddns/bivor/group14_inclass07/MainActivity.java
+++ Group14_InClass07/app/src/main/java/net/ddns/bivor/group14_inclass07/MainActivity.java
@@ -3,11 +3,17 @@ package net.ddns.bivor.group14_inclass07;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+
 public class MainActivity extends AppCompatActivity {
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
         setContentView(R.layout.activity_main);
+        setTitle("Expense App");
     }
 }
