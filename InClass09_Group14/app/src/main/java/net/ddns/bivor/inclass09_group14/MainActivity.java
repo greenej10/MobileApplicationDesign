7ea7cb4126d0cff4990b57252719c21db0cfb8bb
Mohaimenur Rahman Bivor <abm.bivor@gmail.com>
2019-04-01 19:26:32 -0400

    inclass09 created

---
+++ InClass09_Group14/app/src/main/java/net/ddns/bivor/inclass09_group14/MainActivity.java
@@ -0,0 +1,13 @@
+package net.ddns.bivor.inclass09_group14;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
