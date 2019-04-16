5b1271f2180d086e2360adf0b2e1d64278464c4c
Mohaimenur Rahman Bivor <abm.bivor@gmail.com>
2019-04-15 20:31:58 -0400

    inclass11 done

--- Group14_InClass11/app/src/main/java/net/ddns/bivor/group14_inclass11/MainActivity.java
+++ Group14_InClass11/app/src/main/java/net/ddns/bivor/group14_inclass11/MainActivity.java
@@ -5,22 +5,34 @@ import android.content.Context;
 import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.pm.PackageManager;
+import android.graphics.Color;
 import android.location.Location;
 import android.location.LocationListener;
 import android.location.LocationManager;
 import android.provider.Settings;
+import android.support.annotation.NonNull;
 import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.widget.Toast;
 
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.MapFragment;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.LatLngBounds;
 import com.google.android.gms.maps.model.MarkerOptions;
+import com.google.android.gms.maps.model.Polyline;
+import com.google.android.gms.maps.model.PolylineOptions;
+import com.google.gson.Gson;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.ArrayList;
+import java.util.Arrays;
 
 public class MainActivity extends AppCompatActivity implements OnMapReadyCallback {
 
@@ -28,6 +40,8 @@ public class MainActivity extends AppCompatActivity implements OnMapReadyCallbac
 
     LocationManager locationManager;
     LocationListener locationListener;
+
+    public  static final int MY_PERMISSIONS_REQUEST = 1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -111,7 +125,25 @@ public class MainActivity extends AppCompatActivity implements OnMapReadyCallbac
         }
     }
 
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
 
+        switch (requestCode){
+            case MY_PERMISSIONS_REQUEST:{
+                if (grantResults.length > 0
+                        && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+
+                    locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 100, 0, locationListener);
+
+                } else {
+                    Toast.makeText(this, "Storage Permission Not Given", Toast.LENGTH_SHORT).show();
+                }
+                return;
+            }
+
+        }
+    }
 
     @Override
     public void onMapReady(GoogleMap googleMap) {
@@ -119,9 +151,69 @@ public class MainActivity extends AppCompatActivity implements OnMapReadyCallbac
 
         LatLng sydney = new LatLng(-34,151);
 
+        String loc = loadJSONFromAsset();
+
+        Gson gson = new Gson();
+
+        net.ddns.bivor.group14_inclass11.Location points = gson.fromJson(loc, net.ddns.bivor.group14_inclass11.Location.class);
+
+        ArrayList<Loc> locations = new ArrayList<>();
+        locations = points.getPoints();
+
+        final ArrayList<LatLng> coordList = new ArrayList<>();
+
+        for(Loc loc1: locations){
+            coordList.add(new LatLng(Double.parseDouble(loc1.getLatitude()),Double.parseDouble(loc1.getLongitude())));
+        }
+
+        Polyline polyline1 = mMap.addPolyline((new PolylineOptions())
+                .clickable(true)
+                .addAll(coordList));
+
+        polyline1.setColor(Color.BLUE);
+
+        LatLng startMarker = new LatLng(coordList.get(0).latitude, coordList.get(0).longitude);
+        LatLng endMarker = new LatLng(coordList.get(coordList.size()-1).latitude, coordList.get(coordList.size()-1).longitude);
+
         mMap.addMarker(new MarkerOptions()
-                .position(sydney)
-                .title("Marker"));
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+                .position(startMarker)
+                .title("Start Marker"));
+
+        mMap.addMarker(new MarkerOptions()
+                .position(endMarker)
+                .title("End Marker"));
+
+
+
+        mMap.setOnMapLoadedCallback(new GoogleMap.OnMapLoadedCallback() {
+            @Override
+            public void onMapLoaded() {
+
+                LatLngBounds.Builder builder = new LatLngBounds.Builder();
+                for(int i=0;i<coordList.size();i++){
+                    builder.include(coordList.get(i));
+                }
+
+                LatLngBounds bounds = builder.build();
+                mMap.moveCamera(CameraUpdateFactory.newLatLngBounds(bounds, 50));
+            }
+        });
+
+    }
+
+    public String loadJSONFromAsset() {
+        String json = null;
+        try {
+            InputStream is = getAssets().open("trip.json");
+            int size = is.available();
+            byte[] buffer = new byte[size];
+            is.read(buffer);
+            is.close();
+            json = new String(buffer, "UTF-8");
+        } catch (IOException ex) {
+            ex.printStackTrace();
+            return null;
+        }
+        return json;
     }
 }
